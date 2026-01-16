f9eeff78d6a76df4e577bdb7392781a568596311
TomsAronietis <tom@webgoalz.com>
2026-01-16 20:58:15 +0200

    Updated index.html

--- src/components/BookingWidget.tsx
+++ src/components/BookingWidget.tsx
@@ -1,14 +1,10 @@
-const CALENDLY_EMBED = `<!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/starburstcraftstudio" style="min-width:320px;height:700px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-<!-- Calendly inline widget end -->`;
-
 export default function BookingWidget() {
   return (
     <div className="bg-white rounded-lg shadow-sm">
       <div
-        className="w-full"
-        dangerouslySetInnerHTML={{ __html: CALENDLY_EMBED }}
+        className="calendly-inline-widget w-full"
+        data-url="https://calendly.com/starburstcraftstudio"
+        style={{ minWidth: '320px', height: '700px' }}
       />
     </div>
   );

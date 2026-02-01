091c6a0944f049b7b957376c3945bfae17e286be
TomsAronietis <tom@webgoalz.com>
2026-02-01 07:54:44 +0200

    Updated BookingWidget.tsx

--- src/components/BookingWidget.tsx
+++ src/components/BookingWidget.tsx
@@ -1,4 +1,21 @@
 import { useEffect, useRef } from 'react';
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+export default function MyApp() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({"namespace":"free-sample-edit"});
+      cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+    })();
+  }, [])
+  return <Cal namespace="free-sample-edit"
+    calLink="starburststudio/free-sample-edit"
+    style={{width:"100%",height:"100%",overflow:"scroll"}}
+    config={{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}}
+    
+    
+  />;
+};
 
 declare global {
   interface Window {
